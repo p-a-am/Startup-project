0355779c786e5beb7518d427aff0f533fa228728
p-a-am <137822451+p-a-am@users.noreply.github.com>
2025-10-03 14:36:38 -0300

    Update ML_training.hs Tons of fixes and reworkings

--- FINAL/Haskell Neural network from scratch/ML_training.hs
+++ FINAL/Haskell Neural network from scratch/ML_training.hs
@@ -29,6 +29,10 @@ activation_function x = 1.0/(1.0 ** (-x))
 complete_neuron:: Float -> Int-> Float
 
 complete_neuron y z = activation_function (sum_neuron (weights (float_to_int y)) (num_of_parameters z))
+--Complete neuron with adjustable wights
+complete_neuron_rework:: [Float] -> Int -> Float
+
+complete_neuron_rework y z = activation_function (sum_neuron y (num_of_parameters z))
 --complete neuron layer with n neurons y to ys weights and z parameters
 neuron_layer:: Int -> [Float] -> Int -> [Float]
 
@@ -47,8 +51,25 @@ neural_network_example:: [Float]
 
 neural_network_example = neuron_layer 1 (neuron_layer_interaction (neuron_layer 8 (weights 24) 24) (neuron_layer_interaction (neuron_layer 1 (weights 3) 3) [] []) []) 3
 -- memorizing and adjusting weights for each neuron
-adjusting_weights_for_each_neuron:: [Float] -> Float -> [Float] -> [Float]
+adjusting_weights_for_each_neuron:: [Float] -> [Float] -> [Int] -> [Float]
 
-adjusting_weights_for_each_neuron (saida:xs) esperado (variavel:zs) = [2 * (saida - esperado) - variavel] ++ adjusting_weights_for_each_neuron (saida:xs) esperado zs
-adjusting_weights_for_each_neuron (saida:xs) esperado [] = []
+adjusting_weights_for_each_neuron (saida:xs) (esperado:ys) (variavel:zs) = [2 * (saida - esperado) - (int_to_float variavel)] ++ adjusting_weights_for_each_neuron (saida:xs) (esperado : ys) zs
+adjusting_weights_for_each_neuron (saida:xs) (esperado:ys) [] = []
+adjusting_weights_for_each_neuron (saida:xs) [] (variavel:zs) = (saida:xs)
+--Neural network reworking the neuron building
+nn_neuron_building :: [Float] -> [Float] -> Int  -> [Float]
 
+nn_neuron_building value (weight:xs) param = adjusting_weights_for_each_neuron [complete_neuron_rework (weight:xs) (param)] value (num_of_parameters param)
+--Neural network reworking the layer where weight is a 2D Matrix of weights for each layer
+nn_layers :: [Float] ->[[Float]] -> Int -> [Float]
+
+nn_layers value ((x:xs):ys) param = nn_neuron_building value (x:xs) param ++ nn_layers value ys param
+nn_layers value [] param = []
+--creating a 2D matrix of random weights
+
+matrix_random_weights:: Int -> Int -> [[Float]]
+matrix_random_weights x y = [weights x] ++ matrix_random_weights (x-0.004387) (y-1)
+--rebuilding and the NN itself (1 - 8 - 1) with random weights and random input
+
+neural_network_example_2:: [Float]
+neural_network_example_2 = nn_layers (nn_layers () ()) (matrix_random_weights 3 1) 1
